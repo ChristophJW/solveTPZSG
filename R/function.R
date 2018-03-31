6b07c56c774b9d7eaed6450b8d2fffb7a5b2fc43
Christoph Wölfle <Christoph@Christophs-MacBook-Air.local>
2018-03-31 14:12:26 +0200

    comments integrated

--- R/function.R
+++ R/function.R
@@ -1,4 +1,12 @@
-game_function <- function(matrix) {
+#' Solve a two-person zero-sum game.
+#' @name solveGame
+#' @param matrix A matrix.
+#' @return The value of the game \code{matrix}, the saddlepoints of the \code{matrix} if existing and the strategies of the players if the strategies are not pure.
+#' @examples
+#' solveGame(matrix(c(10,2,2,10,4,8,1,12), ncol = 2, byrow = TRUE))
+#' solveGame(matrix(c(-3,1,2,0,5,2,3,2,2,-3,1,-3,1,0,-2,2), nrow = 4, byrow = TRUE))
+
+solveGame <- function(matrix) {
   if(!is.matrix(matrix)){
     stop("Please enter a matrix!")
   }
@@ -29,9 +37,10 @@ game_function <- function(matrix) {
   return(out)
 }
 
-generateMatrixFromSaddleVector <- function(vector){
-  return(matrix(vector, ncol = 2, byrow = TRUE))
-}
+#' Find the maximum of the rows minima.
+#' @name getMaxOfRowMin
+#' @param matrix A matrix
+#' @return The maximum of the rows minima as numeric.
 
 getMaxOfRowMin <- function(matrix) {
   numRow = nrow(matrix)
@@ -42,6 +51,11 @@ getMaxOfRowMin <- function(matrix) {
   return(max(minRowV))
 }
 
+#' Find the minimum of the columns maxima.
+#' @name getMinOfColMax
+#' @param matrix A matrix
+#' @return The minimum of the columns maxima as numeric.
+
 getMinOfColMax <- function(matrix) {
   numCol = ncol(matrix)
   maxColV = c();
@@ -50,6 +64,12 @@ getMinOfColMax <- function(matrix) {
   }
   return(min(maxColV))
 }
+
+#' Find the saddlepoints of the game.
+#' @name getSaddlePointsOfGame
+#' @param matrix A matrix
+#' @param maxCol A numeric
+#' @return The matrix of saddlepoints.
 
 getSaddlePointsOfGame <- function(matrix, maxCol) {
   numCol = ncol(matrix)
@@ -68,8 +88,23 @@ getSaddlePointsOfGame <- function(matrix, maxCol) {
       }
     }
   }
-  return(saddle)
+  return(generateMatrixFromSaddleVector(saddle))
 }
+
+#' Generates a mx2 matrix from the saddlepoints vector.
+#' @name generateMatrixFromSaddleVector
+#' @param vector A vector.
+#' @return The matrix of saddlepoints formated mx2 from \code{vector}.
+
+generateMatrixFromSaddleVector <- function(vector){
+  return(matrix(vector, ncol = 2, byrow = TRUE))
+}
+
+#' Solve the linear program of the gamematrix
+#' @name solveLinearProgram
+#' @param matrix A matrix.
+#' @param minmax A string.
+#' @return The max or min value of the game and the probabilities of the players strategies.
 
 solveLinearProgram <- function(matrix, minmax){
   ncol = ncol(matrix)
@@ -93,63 +128,13 @@ solveLinearProgram <- function(matrix, minmax){
   return(lp(minmax, f.obj, f.con, f.dir, f.rhs)$solution)
 }
 
-computeIntersection <- function(a, b, c, d){
-  if((a-c-b+d) == 0)
-    return(NA)
-  return((d-b)/(a-c-b+d))
-}
+#' Plot the graphical solution of a 2xn or mx2 matrixgame
+#' @name plotSolution
+#' @param matrix A matrix.
+#' @param rowOrCol A string.
+#' @return The plot of the solution in a cartesian coordinate system with a legend.
 
-drawLine <- function(x, a, b) {
-  return(a*x + b*(1-x))
-}
-
-drawMinOrMaxLine <- function(matrix, minmax){
-  intersections <- getAllIntersections(matrix)
-  values = getMinOrMaxFunctionsValueForIntersections(matrix, intersections, minmax)
-  offsetDataFrame <- data.frame(x=values[,1], y=values[,2], xend=values[,3], yend=values[,4])
-  return(offsetDataFrame)
-}
-
-getAllIntersections <- function(matrix){
-  intersections <- c()
-  index <- 1
-  for(i in 1:(nrow(matrix))){
-    for(j in 1:(nrow(matrix))){
-      intersection <- computeIntersection(matrix[i,1], matrix[i,2], matrix[j,1], matrix[j,2])
-      if(!(is.na(intersection) || is.element(intersection, intersections))) {
-        intersections[index] <- intersection
-        index <- index + 1
-      }
-    }
-  }
-  intersections[index] = 0
-  intersections[index+1] = 1
-  return(sort(intersections))
-}
-
-getMinOrMaxFunctionsValueForIntersections <- function(matrix, intersections, minmax){
-  functionValuesForIntersection <- c()
-  minOrMaxFunctionsValue <- matrix(NA, ncol = 4, nrow = length(intersections))
-  for(i in 1:length(intersections)){
-    for(j in 1:nrow(matrix)){
-      functionValuesForIntersection[j] <- drawLine(intersections[i], matrix[j,1], matrix[j,2])
-    }
-    minOrMaxFunctionsValue[i,1] <- intersections[i]
-    if(minmax == 'max'){
-      minOrMaxFunctionsValue[i,2] <- min(functionValuesForIntersection)
-    } else {
-      minOrMaxFunctionsValue[i,2] <- max(functionValuesForIntersection)
-    }
-  }
-  for (i in 1:nrow(minOrMaxFunctionsValue)-1) {
-    minOrMaxFunctionsValue[i,3] <-  minOrMaxFunctionsValue[i+1,1]
-    minOrMaxFunctionsValue[i,4] <-  minOrMaxFunctionsValue[i+1,2]
-  }
-  return(minOrMaxFunctionsValue[-nrow(minOrMaxFunctionsValue),])
-}
-
-plotSolution <- function(matrix){
-  print(matrix)
+plotSolution <- function(matrix, rowOrCol='row'){
   if(isMatrixOfRightDimention(matrix)){
     stop("Enter 2xn or mx2 matrix!")
   }
@@ -158,6 +143,17 @@ plotSolution <- function(matrix){
     minOrMax <- 'max'
     solveLP = solveLinearProgram(matrix, minOrMax)
     matrix = t(matrix)
+  } else if(ncol(matrix) == nrow(matrix)){
+    if(rowOrCol == 'row'){
+      minOrMax <- 'max'
+      solveLP = solveLinearProgram(matrix, minOrMax)
+      matrix = t(matrix)
+    } else if(rowOrCol == 'col') {
+      minOrMax <- 'min'
+      solveLP = solveLinearProgram(matrix, minOrMax)
+    } else {
+      stop("Enter row or col for param!")
+    }
   } else {
     minOrMax <- 'min'
     solveLP = solveLinearProgram(matrix, minOrMax)
@@ -177,12 +173,12 @@ plotSolution <- function(matrix){
   if(minOrMax == 'min') {
     p <- p + ggplot2::geom_segment(
                     data=drawMinOrMaxLine(matrix, minOrMax),
-                    aes(x = x, y = y, xend = xend, yend = yend, colour = "max payment")
+                    aes(x = x, y = y, xend = xend, yend = yend, colour = "max payment col player")
                     )
   } else {
     p <- p + ggplot2::geom_segment(
                     data=drawMinOrMaxLine(matrix, minOrMax),
-                    aes(x = x, y = y, xend = xend, yend = yend, colour = "min payoff")
+                    aes(x = x, y = y, xend = xend, yend = yend, colour = "min payoff row player")
                     )
   }
   p <- p + ggplot2::geom_hline(
@@ -202,9 +198,19 @@ plotSolution <- function(matrix){
   return(p)
 }
 
+#' Check if the matrix has the right format of 2xn or mx2
+#' @name isMatrixOfRightDimention
+#' @param matrix A matrix.
+#' @return The TRUE if the matrix is of the right format or FALSE if not.
+
 isMatrixOfRightDimention <- function(matrix) {
   return(!is.matrix(matrix) || (dim(matrix)[1] > 2 & dim(matrix)[2] > 2))
 }
+
+#' Generates random colors for the linear functions
+#' @name generateColors
+#' @param numOfRows A integer.
+#' @return The colors for the linear functions.
 
 generateColors <- function(numOfRows) {
   hues = seq(15, 375, length = numOfRows + 1)
@@ -213,6 +219,103 @@ generateColors <- function(numOfRows) {
   colors <- unlist(strsplit(colors, " "))
 }
 
+#' Generates name for the linear function
+#' @name generateFunctionName
+#' @param i A integer.
+#' @param matrix A matrix.
+#' @return The name of the linear function as a string.
+
 generateFunctionName <- function(i, matrix){
   return(paste("e",i,"=",matrix[i,1],"*x +",matrix[i,2],"*(1-x)" ))
+}
+
+#' Find the intersections of two functions
+#' @name computeIntersection
+#' @param a A numeric.
+#' @param b A numeric.
+#' @param c A numeric.
+#' @param d A numeric.
+#' @return The intersection of the two functions or NA if there is not one.
+
+computeIntersection <- function(a, b, c, d){
+  if((a-c-b+d) == 0)
+    return(NA)
+  return((d-b)/(a-c-b+d))
+}
+
+#' Compute the function value f(x) of a function
+#' @name computeLinearFunction
+#' @param x A numeric.
+#' @param a A numeric.
+#' @param b A numeric.
+#' @return The function value f(x) of a function.
+
+computeLinearFunction <- function(x, a, b) {
+  return(a*x + b*(1-x))
+}
+
+#' Draw the line of the max or min of all functions
+#' @name drawMinOrMaxLine
+#' @param matrix A matrix.
+#' @param minmax A string.
+#' @return The data frame of the segments of the min or max function.
+
+drawMinOrMaxLine <- function(matrix, minmax){
+  intersections <- getAllIntersections(matrix)
+  values = getMinOrMaxFunctionsValueForIntersections(matrix, intersections, minmax)
+  offsetDataFrame <- data.frame(x=values[,1], y=values[,2], xend=values[,3], yend=values[,4])
+  return(offsetDataFrame)
+}
+
+#' Find all intersections of the functions
+#' @name getAllIntersections
+#' @param matrix A matrix.
+#' @return The function values of the functions intersections in sorted vector.
+
+getAllIntersections <- function(matrix){
+  intersections <- c()
+  index <- 1
+  for(i in 1:(nrow(matrix))){
+    for(j in 1:(nrow(matrix))){
+      intersection <- computeIntersection(matrix[i,1], matrix[i,2], matrix[j,1], matrix[j,2])
+      if(!(is.na(intersection) || is.element(intersection, intersections))) {
+        intersections[index] <- intersection
+        index <- index + 1
+      }
+    }
+  }
+  intersections[index] = 0
+  intersections[index+1] = 1
+  return(sort(intersections))
+}
+
+#' Find the min or max functions value for each intersection
+#' @name getMinOrMaxFunctionsValueForIntersections
+#' @param matrix A matrix.
+#' @param intersections A vector.
+#' @param minmax A string.
+#' @return The vector of the intersection min/max function value tupel.
+
+getMinOrMaxFunctionsValueForIntersections <- function(matrix, intersections, minmax){
+  functionValuesForIntersection <- c()
+  minOrMaxFunctionsValue <- matrix(NA, ncol = 4, nrow = length(intersections))
+  for(i in 1:length(intersections)){
+    for(j in 1:nrow(matrix)){
+      functionValuesForIntersection[j] <- computeLinearFunction(
+                      intersections[i],
+                      matrix[j,1],
+                      matrix[j,2])
+    }
+    minOrMaxFunctionsValue[i,1] <- intersections[i]
+    if(minmax == 'max'){
+      minOrMaxFunctionsValue[i,2] <- min(functionValuesForIntersection)
+    } else {
+      minOrMaxFunctionsValue[i,2] <- max(functionValuesForIntersection)
+    }
+  }
+  for (i in 1:nrow(minOrMaxFunctionsValue)-1) {
+    minOrMaxFunctionsValue[i,3] <-  minOrMaxFunctionsValue[i+1,1]
+    minOrMaxFunctionsValue[i,4] <-  minOrMaxFunctionsValue[i+1,2]
+  }
+  return(minOrMaxFunctionsValue[-nrow(minOrMaxFunctionsValue),])
 }
